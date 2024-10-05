3a50963e73a0a286c4e1259a23aa15352ef39e93
Lizaveta01 <goldglaider@gmail.com>
2024-10-05 13:28:33 +0300

    feat: add fs solution

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,23 @@
+import { readdir, mkdir, copyFile } from "node:fs/promises";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+    const sourse = __dirname + "/files";
+    const destination = __dirname + "/files_copy";
+  
+    try{
+      await mkdir(destination);
+      const dirFiles = await readdir(sourse);
+  
+      await Promise.all(dirFiles.map(file => copyFile(`${sourse}/${file}`, `${destination}/${file}`)));
+  
+    } catch(e){
+      throw new Error('FS operation failed: ' + e.message);
+    }
 };
 
 await copy();
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,12 @@
 const read = async () => {
-    // Write your code here 
+    const src = getPath(import.meta.url, "fileToRead.txt");
+
+    try {
+        const content = await readFile(src, {encoding : "utf8"})
+        console.log(content);
+    } catch (e) {
+        throw new Error('FS operation failed: ' + e.message);
+    }
 };
 
 await read();
