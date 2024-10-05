41b30a13bebd8dd777c4bd5e42c44c19e4752466
Lizaveta01 <goldglaider@gmail.com>
2024-10-05 13:54:40 +0300

    feat: add solution for hash, cli, modules

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    const variables = process.argv;
+    const targetVariables = variables.slice(2);
+    const res = [];
+    for(let i =0; i<targetVariables.length; i++) {
+        res.push(`${targetVariables[i].slice(2)} is ${targetVariables[i+1]}`);
+        i++;
+    }
+    console.log(res.join(', '));
 };
 
 parseArgs();
